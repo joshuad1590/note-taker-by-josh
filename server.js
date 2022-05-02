517cea34d752ec057fbe255e53dcbb9e31296ae7
joshuad1590 <joshuad15@gmail.com>
2022-05-02 19:17:34 -0400

    added GET

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const fs = require('fs');
+const path = require('path');
+
+const notes = require('./Develop/db/db.json');
 
 
+app.get('/api/notes', (req, res) => {
+    res.json(notes.slice(1));
+});
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server listening on ${PORT}!`);
